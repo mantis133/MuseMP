1c9ffe505d4733b3ec07596227eb7f75d6066dc7
mantis133 <mantisshrimp40@gmail.com>
2025-02-16 22:52:33 +1300

    started on the actions record table

--- composeApp/src/androidMain/kotlin/org/mantis/muse/storage/entity/PlayRecord.kt
+++ composeApp/src/androidMain/kotlin/org/mantis/muse/storage/entity/PlayRecord.kt
@@ -2,6 +2,19 @@ package org.mantis.muse.storage.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.sql.Date
+
+enum class RecordableActions {
+    Play,
+    Pause,
+    SkipNext,
+    SkipLast,
+    Started,
+    Ended,
+    ChangedLoopState,
+    ChangedShuffleState,
+    SongLoaded,
+}
 
 @Entity(tableName = "songRecords")
 data class PlayRecord(
@@ -9,5 +22,7 @@ data class PlayRecord(
     val songName: String,
     val songArtists: String,
     val playlistName: String?,
-    val listenedDurationSecs: Int
+    val songDurationPositionMS: Int,
+    val dateTime: Date,
+    val action: RecordableActions
 )
