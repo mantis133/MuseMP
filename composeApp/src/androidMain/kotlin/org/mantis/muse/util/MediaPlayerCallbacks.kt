f3a3dba4e4fbaaeff2d52ec07e9aa22bd847e21e
mantis133 <mantisshrimp40@gmail.com>
2025-02-16 22:51:39 +1300

    removed unused MediaPlayerCallbacks.kt

--- composeApp/src/androidMain/kotlin/org/mantis/muse/util/MediaPlayerCallbacks.kt
+++
@@ -1,9 +0,0 @@
-package org.mantis.muse.util
-
-data class MediaPLayerCallbacks (
-    val playPause: (isPlaying: Boolean) -> Unit,
-    val skipNext: () -> Unit,
-    val skipLast: () -> Unit,
-    val loop: () -> Unit,
-    val shuffle: () -> Unit
-)
