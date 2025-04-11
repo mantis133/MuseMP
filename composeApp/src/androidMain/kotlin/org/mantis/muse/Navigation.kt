644e818ef435c284cf58451614c72fef7298386e
mantis133 <mantisshrimp40@gmail.com>
2025-04-11 21:28:34 +1200

    added the horizontal navigation component

--- composeApp/src/androidMain/kotlin/org/mantis/muse/Navigation.kt
+++ composeApp/src/androidMain/kotlin/org/mantis/muse/Navigation.kt
@@ -1,20 +1,28 @@
 package org.mantis.muse
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import kotlinx.serialization.Serializable
 import org.mantis.muse.layouts.PlaylistSelectionScreenState
+import org.mantis.muse.layouts.components.HorizontalNavView
 import org.mantis.muse.layouts.components.ScrollableNavigation
 
 @Serializable sealed class Screen(val display: String) {
+    @Serializable data object Home: Screen("Home")
     @Serializable data object MusicPlayer : Screen("Player")
     @Serializable data object PlaylistSelectionScreen : Screen("Playlists")
+    @Serializable data class SinglePlaylistViewScreen(val playlistURI: String): Screen("")
     @Serializable data object SongSelectionScreen : Screen("Songs")
+    @Serializable data object ArtistsScreen : Screen("Artists")
 }
 
 @Composable
@@ -23,40 +31,57 @@ fun NavHostContainer(
 ) {
     val navController = rememberNavController()
 
-    val navigatableScreens = listOf(
+    val navigableScreens = listOf(
+        Screen.Home,
         Screen.PlaylistSelectionScreen,
-        Screen.SongSelectionScreen
+        Screen.SongSelectionScreen,
+        Screen.ArtistsScreen
     )
+    Column {
+        val fontStyling = TextStyle.Default.copy(fontSize = 50.sp, color = Color.White)
 
-    NavHost(navController = navController, startDestination = Screen.PlaylistSelectionScreen, modifier){
-        composable<Screen.MusicPlayer>{
-//            PlayerUI(
-//                callbacks = mediaCallbacks,
-//                modifier = Modifier
-//                    .height(400.dp)
-//            )
-        }
-        composable<Screen.PlaylistSelectionScreen>{
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-            ){
-                ScrollableNavigation(
-                    routes = navigatableScreens,
-                    navHost = navController
-                )
-                PlaylistSelectionScreenState(
-                    modifier = Modifier
-                        .padding(10.dp)
-                )
+        HorizontalNavView(
+            navigableScreens,
+            navHost = navController,
+            fontStyling = fontStyling,
+            modifier = Modifier
+                .fillMaxHeight(0.1f)
+                .background(Color.Black)
+        )
+        NavHost(navController = navController, startDestination = Screen.PlaylistSelectionScreen, modifier){
+            composable<Screen.MusicPlayer>{
+    //            PlayerUI(
+    //                callbacks = mediaCallbacks,
+    //                modifier = Modifier
+    //                    .height(400.dp)
+    //            )
             }
-        }
-        composable<Screen.SongSelectionScreen> {
-            Column {
-                ScrollableNavigation(
-                    routes = navigatableScreens,
-                    navHost = navController
-                )
+            composable<Screen.Home>{
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+
+                }
+            }
+            composable<Screen.PlaylistSelectionScreen>{
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                ){
+                    PlaylistSelectionScreenState(
+                        modifier = Modifier
+                            .padding(10.dp)
+                    )
+                }
+            }
+            composable<Screen.SongSelectionScreen> {
+                Column {
+
+                }
+            }
+            composable<Screen.ArtistsScreen> {
+
             }
         }
     }
